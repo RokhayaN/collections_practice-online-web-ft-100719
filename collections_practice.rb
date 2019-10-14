74eb67ea02f8dc71dccc4e3416568db684e53a50
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-14 16:11:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def
+sort_array_asc = [5,3,1,4,2]
+  a = [5,3,1,4,2]
+  a.sort {|x,y| -(x <=> y)}
+end
 
+def 
+  sort_array_desc= [5,3,1,4,2]
+  a = [5,3,1,4,2]
+  a.sort {|x,y| (x <=> y)}
+end
+  
+  def  sort_array_char_count 
+    a =["a","d","c"]
+    a.sort
+  end 
+    
